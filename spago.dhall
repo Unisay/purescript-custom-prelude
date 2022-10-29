84759272b34778c1df38803d836694d60b3f4085
Yura Lazarev <Unisay@users.noreply.github.com>
2022-10-29 13:25:50 +0200

    Tuples and mapmaps

--- spago.dhall
+++ spago.dhall
@@ -1,5 +1,5 @@
 { name = "purescript-custom-prelude"
-, dependencies = [ "debug", "either", "maybe", "prelude", "strings" ]
+, dependencies = [ "debug", "either", "maybe", "prelude", "strings", "tuples" ]
 , packages = ./packages.dhall
 , sources = [ "src/**/*.purs" ]
 }
--- src/Prelude/Custom.purs
+++ src/Prelude/Custom.purs
@@ -3,11 +3,17 @@ module Custom.Prelude
   , module Data.Maybe
   , module Data.Either
   , module Debug
+  , module Tuple
+  , module NTuple
   , pass
   , unwords
   , unlines
   , (|>)
   , (<|)
+  , mapmap
+  , (<<$>>)
+  , mapmapFlipped
+  , (<<#>>)
   ) where
 
 import Prelude
@@ -16,14 +22,9 @@ import Data.Either (Either(..), either, hush, note)
 import Data.Function as Function
 import Data.Maybe (Maybe(..), fromMaybe, maybe)
 import Data.String as String
-import Debug
-  ( class DebugWarning
-  , spy
-  , spyWith
-  , trace
-  , traceM
-  , traceTime
-  )
+import Data.Tuple (Tuple(..), curry, fst, snd, swap, uncurry) as Tuple
+import Data.Tuple.Nested (type (/\), (/\)) as NTuple
+import Debug (class DebugWarning, spy, spyWith, trace, traceM, traceTime)
 
 pass ∷ ∀ f. Applicative f ⇒ f Unit
 pass = pure unit
@@ -37,3 +38,14 @@ unlines = String.joinWith "\n"
 infixr 0 Function.apply as <|
 
 infixl 1 Function.applyFlipped as |>
+
+mapmap ∷ ∀ f g a b. Functor g ⇒ Functor f ⇒ (a → b) → f (g a) → f (g b)
+mapmap = map >>> map
+
+infixl 4 mapmap as <<$>>
+
+mapmapFlipped
+  ∷ ∀ f g a b. Functor g ⇒ Functor f ⇒ f (g a) → (a → b) → f (g b)
+mapmapFlipped f a = mapmap a f
+
+infixl 1 mapmapFlipped as <<#>>
