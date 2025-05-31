a677f780fb7b0455f5282a8d902218a676ce1050
Yura Lazarev <Unisay@users.noreply.github.com>
2025-05-31 16:23:40 +0200

    Refactor flake configuration: remove unused inputs and update purescript-overlay integration

--- flake.nix
+++ flake.nix
@@ -2,33 +2,56 @@
   description = "Purescript Flake";
 
   inputs = {
-    flake-compat.flake = false;
-    flake-compat.url = "github:edolstra/flake-compat";
-    flake-utils.url = "github:numtide/flake-utils";
-    nixpkgs.url = "nixpkgs/nixos-unstable";
-    easy-purescript-nix = {
-      url = "github:justinwoo/easy-purescript-nix";
-      flake = false;
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    purescript-overlay = {
+      url = "github:thomashoneyman/purescript-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = { self, nixpkgs, flake-utils, flake-compat, easy-purescript-nix }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = nixpkgs.legacyPackages.${system};
-        eps = import easy-purescript-nix { inherit pkgs; };
-      in {
-        devShell = pkgs.mkShell {
-          buildInputs = with pkgs; [
-            dhall
-            nixfmt
-            # nodejs-19_x
-            purescript
-            eps.purs-tidy
-            eps.spago
-            treefmt
-          ];
-        };
-      });
-}
+  outputs =
+    { self, nixpkgs, ... }@inputs:
+    let
+      supportedSystems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
 
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+
+      nixpkgsFor = forAllSystems (
+        system:
+        import nixpkgs {
+          inherit system;
+          config = { };
+          overlays = builtins.attrValues self.overlays;
+        }
+      );
+    in
+    {
+      overlays = {
+        purescript = inputs.purescript-overlay.overlays.default;
+      };
+      devShells = forAllSystems (
+        system:
+        # pkgs now has access to the standard PureScript toolchain
+        let
+          pkgs = nixpkgsFor.${system};
+        in
+        {
+          default = pkgs.mkShell {
+            name = "my-purescript-project";
+            buildInputs = with pkgs; [
+              purs
+              spago
+              purs-tidy-bin.purs-tidy-0_10_0
+              treefmt
+              dhall
+            ];
+          };
+        }
+      );
+    };
+}
